a664fe91df09453ca1bb780ead64383992d9353d
jorge cotrado <jcotrado@senati.pe>
2024-05-15 17:51:36 -0500

    ejemplo de bucles

---
+++ 07-bucles.php
@@ -0,0 +1,24 @@
+<?php  
+    $contador=1;
+    $sumanotas=0;
+    while($contador<=3) {        
+        echo "Ingrese su Notas : ";
+        $nota =  fgets(STDIN);
+        $sumanotas = $sumanotas + $nota;
+        $contador++;//$contador=$contador+1
+    }
+    $notapromedio = $sumanotas/3;
+    echo "\n El promedio de notas es :".$notapromedio."\n";
+
+    //logica que me permite dar el mayor de 5 numeros
+    $mayor=0;
+    for($i=1;$i<=5;$i++) {
+        echo "Ingrese un numero : ";
+        $numero =  fgets(STDIN);      
+        if($mayor<$numero) {
+            $mayor=$numero;
+        }
+    }
+
+    echo "El numero mayor es :".$mayor;
+?>
