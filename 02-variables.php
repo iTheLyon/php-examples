58c0d76124e40260aa80934c062eb96356faa38f
jorge cotrado <jcotrado@senati.pe>
2024-05-15 16:39:32 -0500

    nuevos ejercicios

---
+++ 02-variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombres ="nicky salomon";
+$apellidos="enriquez torres";
+$curso="BACKEND DEVELOPER WEB";
+$semestre="TERCER SEMESTRE";
+//juntar nombres apellidos curso y semestre
+echo $nombres." ".$apellidos." ".$curso." ".$semestre;
+
+?>
